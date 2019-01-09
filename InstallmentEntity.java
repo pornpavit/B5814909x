82c5f74f8972d9dd6279191908b2e22f60f7d99d
pornpavit <toompornpavit@gmail.com>
2019-01-09 14:06:53 +0700

    add installmentEntity.java - close #4

---
+++ InstallmentEntity.java
@@ -0,0 +1,5 @@
+package installmentEntity;
+	public class InstallmentEntity{
+		private String id;
+		private String name;
+}
---
+++ Sales.java
@@ -0,0 +1,5 @@
+package sales;
+	public class sales{
+		private String id;
+		private String name;
+}
